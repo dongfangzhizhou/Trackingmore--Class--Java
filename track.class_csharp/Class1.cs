fc06c3a211a127fee23f9bb3532977df7d352cc0
dongfangzhizhou <service@trackingmore.com>
2016-10-29 17:15:33 +0800

    use c# to use trackingmore's API

---
+++ track.class_csharp/Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace API
+{
+    public class Tracker
+    {
+        // the apikey you get from trackingmore
+        private string ApiKey = "YOUR API KEY";
+        
+
+        public string getOrderTracesByJson(string requestData, string urlStr, string method)
+        {
+            string result = null;
+            if (method.Equals("post"))
+            {   
+                string ReqURL = "http://api.trackingmore.com/v2/trackings/post";
+                string RelUrl = ReqURL + urlStr;
+                result = sendPost(ReqURL, requestData,"POST");
+            }
+            else if (method.Equals("get"))
+            {
+                string ReqURL = "http://api.trackingmore.com/v2/trackings/get";
+                string RelUrl = ReqURL + urlStr;
+                //Console.WriteLine("RelUrl:" + RelUrl);
+                result = sendPost(RelUrl, requestData, "GET");
+            }
+            else if (method.Equals("batch"))
+            {
+                string ReqURL = "http://api.trackingmore.com/v2/trackings/batch";
+                string RelUrl = ReqURL + urlStr;
+                //Console.WriteLine("RelUrl:" + RelUrl);
+                result = sendPost(RelUrl, requestData, "POST");
+            }
+            else if (method.Equals("codeNumberGet"))
+            {
+                
+                string ReqURL = "http://api.trackingmore.com/v2/trackings";
+                string RelUrl = ReqURL + urlStr;
+                //Console.WriteLine("RelUrl:" + RelUrl);
+                result = sendPost(RelUrl, requestData, "GET");
+            }
+            else if (method.Equals("codeNumberPut"))
+            {
+
+                string ReqURL = "http://api.trackingmore.com/v2/trackings";
+                string RelUrl = ReqURL + urlStr;
+                //Console.WriteLine("RelUrl:" + RelUrl);
+                result = sendPost(RelUrl, requestData, "PUT");
+            }
+            else if (method.Equals("codeNumberDel"))
+            {
+
+                string ReqURL = "http://api.trackingmore.com/v2/trackings";
+                string RelUrl = ReqURL + urlStr;
+                //Console.WriteLine("RelUrl:" + RelUrl);
+                result = sendPost(RelUrl, requestData, "DELETE");
+            }
+
+            else if (method.Equals("realtime"))
+            {
+
+                string ReqURL = "http://api.trackingmore.com/v2/trackings/realtime";
+                string RelUrl = ReqURL + urlStr;
+                //Console.WriteLine("RelUrl:" + RelUrl);
+                result = sendPost(RelUrl, requestData, "POST");
+            }
+
+            return result;
+            
+           
+            
+        }
+
+
+        private string sendPost(string url, string requestData,string method)
+        {
+            string result = "";
+            byte[] byteData = null;
+            if (requestData !=null)
+            {
+                 byteData = Encoding.GetEncoding("UTF-8").GetBytes(requestData.ToString());
+            }
+          
+            try
+            {
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Timeout = 30 * 1000;
+                request.Method = method;
+                request.Headers["Trackingmore-Api-Key"] = ApiKey;
+                
+                if (byteData != null)
+                {
+                    Stream stream = request.GetRequestStream();
+                    stream.Write(byteData, 0, byteData.Length);
+                    stream.Flush();
+                    stream.Close();
+                }
+                
+                
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream backStream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(backStream, Encoding.GetEncoding("UTF-8"));
+                result = sr.ReadToEnd();
+                sr.Close();
+                backStream.Close();
+                response.Close();
+                request.Abort();
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            return result;
+        }
+    }
+}
+
+
+
---
+++ track.class_csharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //post
+            string urlstr = null;
+            string requestdata = "{\"tracking_number\": \"1047435553720\",\"carrier_code\":\"china-ems\"}";
+            string result = new Tracker().getOrderTracesByJson(requestdata, urlstr, "post");
+
+
+            //get
+            string urlStr = "?page=1&limit=25&created_at_min=1441314361&created_at_max=1477308420";
+            string requestData = null;
+            String result = new Tracker().getOrderTracesByJson(requestData, urlStr, "get");
+
+
+            //batch
+            string urlstr = null;
+            string requestdata = "[{\"tracking_number\": \"1047435554520\",\"carrier_code\":\"china-ems\"},{\"tracking_number\": \"1047435555420\",\"carrier_code\":\"china-ems\"}]";
+            string result = new Tracker().getOrderTracesByJson(requestdata, urlstr, "batch");
+
+            //codeNumberGet
+            string urlStr = "/wishpost/82196269752";
+            string requestData = null;
+            String result = new Tracker().getOrderTracesByJson(requestData, urlStr, "codeNumberGet");
+
+
+            //codeNumberPut
+            string urlStr = "/wishpost/82196269752";
+            string requestData = "{\"title\": \"test\",\"customer_name\":\"abner\",\"customer_email\":\"1421851519@qq.com\",\"order_id\":\"#123123\"}";
+            String result = new Tracker().getOrderTracesByJson(requestData, urlStr, "codeNumberPut");
+
+            //codeNumberDel
+            string urlStr = "/4px/RF421899730CN";
+            string requestData = null;
+            String result = new Tracker().getOrderTracesByJson(requestData, urlStr, "codeNumberDel");
+
+            //realtime
+            string urlstr = null;
+            string requestdata = "{\"tracking_number\": \"RF421899728SG\",\"carrier_code\":\"4px\"}";
+            String result = new Tracker().getOrderTracesByJson(requestdata, urlstr, "realtime");
+
+
+            //Console.WriteLine("result:" + result);
+        }
+    }
+}
